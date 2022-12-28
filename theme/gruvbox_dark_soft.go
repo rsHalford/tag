400636614fd5ba2853cdfb8b6755b0a67fe0f772
Richard Halford <dev@rshalford.com>
2022-12-28 17:52:33 +0000

    theme: gruvbox dark soft

---
+++ theme/gruvbox_dark_soft.go
@@ -0,0 +1,7 @@
+package theme
+
+import (
+	"image/color"
+)
+
+var GRUVBOX_DARK_SOFT_BASE = color.RGBA{0x32, 0x30, 0x2F, 0xFF} // bg
--- theme/select.go
+++ theme/select.go
@@ -19,6 +19,12 @@ func Select(theme string) (color.RGBA, []color.RGBA) {
 
 		return base, schema
 
+	case "gruvbox_dark_soft":
+		base := GRUVBOX_DARK_SOFT_BASE
+		schema := GRUVBOX_DARK_COLORS
+
+		return base, schema
+
 	case "gruvbox_light":
 		base := GRUVBOX_LIGHT_BASE
 		schema := GRUVBOX_LIGHT_COLORS
